2a87fefb2094a90f55ab6021d4294887a30d1055
albaforce <raoufusma500@gmail.com>
2024-12-09 11:02:18 +0100

    Exercice4

---
+++ Exercice4.py
@@ -0,0 +1,12 @@
+P1 = int(input("Please type in the age of the first person: "))
+if P1 <= 0:
+        print("Error! Enter a valid age")
+        exit()
+P2 = int(input("Please type in the age of the second person: "))
+if P2 <= 0:
+        print("Error! Enter a valid age")
+        exit()
+if P1 > P2 :
+    print(f"The older age is: {P1}")
+else :
+    print(f"The older age is: {P2}")
