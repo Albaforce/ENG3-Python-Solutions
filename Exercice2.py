3dd026d5db0ad51c5e4d32c9b0703cbdbc2815fa
albaforce <raoufusma500@gmail.com>
2024-12-09 10:45:09 +0100

    Exercice2

---
+++ Exercice2.py
@@ -0,0 +1,12 @@
+Temperature = int(input("Please type in the temperature: "))
+
+if Temperature < 0 :
+    print("It's freezing!")
+
+if Temperature < 10 :
+    print("It's cold!")
+
+if Temperature < 20 :
+    print("It's cool!")
+
+print("Stay safe!")
