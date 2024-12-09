03670316b9bd457aad9db75615ed76ebdc77bba7
albaforce <raoufusma500@gmail.com>
2024-12-09 11:35:22 +0100

    exercice6

--- Exercice6.py
+++ Exercice6.py
@@ -1,16 +1,11 @@
-age1 = int(input("Please type in the age of the first person: "))
-if age1 <= 0 :
-    print("Error age ")
-    exit()
+price = float(input("Please type in a price: "))
 
-age2 = int(input("Please type in the age of the second person: "))
-if age2 <= 0 :
-    print("Error age ")
+if price < 0 : 
+    print("Invalid price ")
     exit()
+    
+dinars = int(price)
+Centimes = round((price - dinars) * 100)
 
-if age1 > age2 :
-    print(f"The older age is:{age1} ")
-elif age2 > age1 :
-    print(f"The older age is:{age2} ")
-else :
-    print("Both people are the same age!")
+print("Dinars: " , dinars)
+print("Centimes: " , Centimes)
