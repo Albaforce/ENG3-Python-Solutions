c582c1da919c2cdf46615835c3f780e335092737
albaforce <raoufusma500@gmail.com>
2025-01-15 17:20:35 +0100

    done

---
+++ Exercice15.py
@@ -0,0 +1,19 @@
+s = input("Please type in a string: ")
+while s == "" :
+    s = input("Please type in a string: ")
+s.lower()
+
+if 'a' in s :
+    print(" a is found")
+else:
+    print(" a is not found")
+
+if 'e' in s :
+    print(" e is found")
+else:
+    print(" e is not found")
+
+if 'o' in s :
+    print(" o is found")
+else:
+    print(" o is not found")
