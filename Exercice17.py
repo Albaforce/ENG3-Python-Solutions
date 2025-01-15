78f2a8ae024f2032ed4a955aa2d942fab410084b
albaforce <raoufusma500@gmail.com>
2025-01-15 18:10:11 +0100

    done

---
+++ Exercice17.py
@@ -0,0 +1,14 @@
+numbers = []
+shoe_sizes  = []
+
+for i in range(1,6):
+    numbers.append(i)
+for i in range(8,13):
+    shoe_sizes.append(i)
+
+print(numbers)
+print(shoe_sizes)  
+
+combining = numbers + shoe_sizes
+
+print(combining)
